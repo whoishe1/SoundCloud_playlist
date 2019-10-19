bcf50817c52d3d5a4d8ca7d41dd21faceb1cc2df
jordan <jordanko9112@gmail.com>
2019-10-19 01:42:26 -0700

    Added retrieve all playlist function and excel function

--- scrape_soundcloud.py
+++ scrape_soundcloud.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 from selenium import webdriver
 import time
+import requests
+import os
 
-#returns playlist in dataframe format
-def getplaylist(this_url):
-    """param this_url: link of playlist url"""
+#Desktop Path
+desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), 'Desktop')
+
+
+#Get PlaylistNames
+def which_playlists(URL):
+    """@param URL: soundcloud playlist url page"""
+    this_url = URL
     driver = webdriver.Chrome()
     try:
         driver.get(this_url)
@@ -14,6 +22,34 @@ def getplaylist(this_url):
         while True:
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        playlist_extensions = soup.find_all("a", class_ = {"soundTitle__title sc-link-dark"})
+        playlist_extensions
+
+        extensions_list = []
+        for ext in playlist_extensions:
+            extensions_list.append(str(ext.get("href")))
+        these_urls = [('https://soundcloud.com' + i) for i in extensions_list]
+    finally:
+        driver.quit()
+        return(these_urls)
+
+#Returns playlist in dataframe format
+def getplaylist(this_url):
+    """param this_url: link of specific playlist url"""
+    driver = webdriver.Chrome()
+    try:
+        driver.get(this_url)
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
             new_height = driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
                 break
@@ -34,11 +70,32 @@ def getplaylist(this_url):
 
         playlist = pd.DataFrame({'Tracks':pd.Series(tracknames_list), 'Artists':pd.Series\
                         (artistnames_list)})
-        
+
     finally:
         driver.quit()
         return(playlist)
 
+#Excel File
+def get_excel(URL):
+    playlist_dfs = []
+    urls = which_playlists(URL)
+    for i in urls:
+        this_df = getplaylist(i)
+        playlist_dfs.append(this_df)
 
-df = getplaylist(this_url = "your_playlist")
+    playlist_order = playlist_dfs[::-1]
+    total_playlist = pd.concat(playlist_order).reset_index(drop = True)
 
+    try:
+        excel_name = '\\soundcloud_playlist'  + "." + "xlsx"
+        writer = pd.ExcelWriter(desktop+str(excel_name), engine = 'xlsxwriter')
+        total_playlist.to_excel(writer, sheet_name = 'total_playlist', index = False)
+        for idx,val in enumerate(playlist_order):
+            idx_ = idx + 1
+            val.to_excel(writer, sheet_name = str(idx_), index =False)
+
+    finally:
+        writer.save()
+
+
+get_excel("https://soundcloud.com/whoishe1/sets")
